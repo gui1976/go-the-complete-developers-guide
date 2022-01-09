96e26f5b778ded2fd98140ced5a2223e03dcde2d
Gui Lopes <gui.lesteves@gmail.com>
2022-01-09 09:33:29 +0000

    deck type and new function print

--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
-	//var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
-	fmt.Println(card)
+	cards := deck{"Ace of Spades", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	cards.print()
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
