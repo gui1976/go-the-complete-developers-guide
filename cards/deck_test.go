466125018430a10bfc1ec9bb974b6be5fe32c68b
Gui Lopes <gui.lesteves@gmail.com>
2022-01-18 23:50:25 +0000

    more tests for go test

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"os"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,26 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 24 {
 		t.Errorf("Expected deck lenght of 24 but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Six of Clubs" {
+		t.Errorf("Expected last card of Six of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+	if len(loadedDeck) != 24 {
+		t.Errorf("Expected deck lenght of 24 but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
